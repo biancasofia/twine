676d9a582a082108f9b8dd3d6389b4b5b39e7cca
biancasofia <sofiabrasil44@gmail.com>
2022-08-15 11:18:41 -0300

    Implementando Transformers

--- twine/ir.py
+++ twine/ir.py
@@ -1,22 +1,157 @@
-from typing import Dict, Union, Tuple, List
+from typing import Dict, Union, Tuple, List, NamedTuple
 from lark import InlineTransformer, Tree
+from lark.visitors import Transformer, v_args
 
-# Representa um módulo Twine
 IR = Dict[str, "Declaration"]
 
-# Representa o lado direito de uma declaração de função
 Declaration = Tuple["ArgDefs", type, "SExpr"]
 
-# A lista de argumentos é uma lista de duplas (nome, tipo) para cada argumento
 ArgDefs = List[Tuple[str, type]]
 
-# Representa uma expressão Twine como S-Expression
 SExpr = Union[list, str, int, bool]
 
 
 def transform(tree: Tree) -> IR:
-    """
-    Transforma uma árvore sintática que descreve um módulo Twine
-    na representação interna do código como um dicionário de definições.
-    """
-    raise NotImplementedError
+   transf = TransformerClass()
+   return transf.transform(tree)
+
+class Declaration(NamedTuple):
+    args: List[Tuple[str, type]] ## arg
+    principal: SExpr
+    returns: str
+
+
+class TransformerClass(Transformer):
+
+      ## define true
+      def true(self):
+         return True
+     ## define false
+      def false(self):
+         return False
+     ## define inteiro
+      def INTEGER(self, p):
+         return int(p)
+     ##define bool 
+      def BOOLEAN(self, children):
+         if (children == "false"):
+            return False
+         elif (children == "true"):
+            return True
+        ## identificador
+      def IDENTIFIER(self, p):
+         return str(p)
+
+        #ou
+      @v_args(inline=True)
+      def or_(self, a, b):
+         return ['|', a, b]
+     #add
+      @v_args(inline=True)
+      def add(self, a, b):
+         return ['+', a, b]
+     #sub
+      @v_args(inline=True)
+      def sub(self, a, b):
+         return ['-', a, b]
+
+     #div
+      @v_args(inline=True)
+      def div(self, a, b):
+         return ['/', a, b]
+   
+      @v_args(inline=True)
+      def and_(self, a, b):
+         return ['^', a, b]
+   
+      @v_args(inline=True)
+      def mul(self, a, b):
+         return ['*', a, b]
+
+
+     #igual
+      @v_args(inline=True)
+      def eq(self, a, b):
+         return ['=', a, b]
+
+     #lt
+      @v_args(inline=True)
+      def lt(self, x, y):
+         return ['<', x, y]
+
+      #bool ou int
+      def TYPE(self, carac):
+            if(carac == "boolean"):
+               return bool
+            else:
+               return int
+
+    #condicao definida
+      @v_args(inline=True)
+      def cond(self, pri, seg, terc):
+          return ["if", pri, seg, terc]
+      
+      #inteiro
+      def integer(self):
+         return int
+
+      @v_args(inline=True)
+      def param(self, name, p_type):
+         if type(p_type) == str:
+            p_type = self.TYPE(p_type)
+         return (str(name),p_type)
+      
+      def params(self, raiz):
+         return raiz
+
+      def args(self, raiz):
+         return raiz
+       #chamada de função
+      @v_args(inline=True)
+      def fcall(self, id, argmap):
+         vet = []
+         vet.append(id)
+         for i in argmap:
+            vet.append(i)
+         return vet
+      
+
+      def principal(self, imprime_exp):
+         vet = []
+         for i in imprime_exp:
+            if(tuple==type(i)):
+              for x in i: 
+               vet.append(x)
+            else:
+               vet.append(i)
+         return vet
+
+
+
+      def imprime_exp(self, raiz):
+         vet = []
+         vet.append("print")
+         for i in raiz:
+            vet.append(i)
+         return tuple(vet)
+
+
+      def exp(self, exp):
+         return exp
+      
+
+      def program(self, def_list):
+         mapa={} 
+         for i in def_list:
+               key=i[0]
+               val=i[1]
+               mapa[key]=val
+         return mapa
+
+
+
+      @v_args(inline=True)
+      def define(self, name, params, type, principal):
+
+         return [name, Declaration(params, type, principal)]
+
