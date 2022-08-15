3ff4aac4bfcd2888a5e1d7f479546ada2a2e7218
biancasofia <sofiabrasil44@gmail.com>
2022-08-15 11:15:50 -0300

    add padrões de twine

--- twine/interpreter.py
+++ twine/interpreter.py
@@ -4,39 +4,74 @@ from typing import Union, Tuple, Dict, List, Iterator, Callable, MutableMapping,
 from .ir import IR, SExpr, Declaration, ArgDefs
 from .stdlib import GLOBALS
 
-# Tipo que para um valor Twine qualquer
-#
-# Incluimos funções junto aos booleanos e inteiros, já que um
-# módulo declara várias funções e as mesmas podem aparecer
-# no dicionário de ambiente.
 Value = Union[bool, int, Callable]
 
-# O ambiente de execução é qualquer coisa que lembre um dicionário
-# Por isso, usamos MutableMapping, no lugar de Dict.
 Env = MutableMapping[str, Value]
 
 
 def eval(sexpr: SExpr, env: Env) -> Value:
-    """
-    Executa uma S-Expression dentro do ambiente dado.
-    """
-    return 42  # ... implemente a versão correta aqui!
+    if type(sexpr) == list:
+        
+        carac = sexpr[0]
+        
+        b = sexpr[2]
+
+        a = eval(sexpr[1], env)
+        #se if
+        if carac == "if":
+            if (len(sexpr) == 4):
+
+                if(sexpr[1] == True):
+
+                        return eval(default_env()["if1"](a,b,sexpr[3]), env)
+                else:
+
+                    return eval(default_env()["if2"](a,sexpr[2],sexpr[3]), env)
+        #ou
+        elif carac == "|":
+            if(sexpr[1] == True):
+
+                return default_env()["|"](a,b)
+
+            else:
+
+                return eval(default_env()["|2"](a,eval(b, env)), env)
+
+        elif carac == "^":
+
+            if(sexpr[1] == False):
+
+                return default_env()["|"](a,b)
+
+            else:
+
+                return eval(default_env()["|2"](a,eval(b, env)), env)
+        else:
+            if (len(sexpr) == 3):
+
+                if carac == "print":
+
+                    print(default_env()[carac](a,eval(b, env)))
+
+                    return b
+            return default_env()[carac](a,eval(b, env)) 
+    if type(sexpr) == bool:
+
+        return bool(sexpr)
+    if type(sexpr) == int:
+        
+        return int(sexpr)
+    
+    else:
+        return NotImplementedError
 
 
 def compile_function(
     argdefs: ArgDefs, restype: type, body: SExpr, env: Env
 ) -> Callable:
-    """
-    Compila a função a partir da listas de argumentos, tipo de retorno, corpo
-    da função e dicionário de ambiente.
-
-    O resultado é uma função Python que realiza a mesma operação
-    codificada na SExpr dentro do contexto de execução fornecido.
-    """
-
-    # Coloque a implementação correta aqui!
+  
     def fn():
-        return ...
+        return 42
 
     return fn
 
@@ -63,21 +98,22 @@ def compile_module(ir: IR, env: Env) -> Dict[str, Callable]:
 
 
 def default_env():
-    """
-    Retorna o ambiente de execução padrão da linguagem.
-
-    Twine é extremamente minimalista e define poucas funções nativas.
-    A única função óbvia é a função print, que recebe um argumento e imprime
-    o resultado sem retornar nada.
-
-    Outras funções menos óbvias são os operadores: +, -, *, etc. Operadores são
-    apenas nomes mais amigáveis para as funções binárias. O dicionário com a biblioteca
-    global fica gravado no ambiente de execução associando por exemplo, "+": lambda x, y: x + y.
-    """
+  
     return {
         "+": lambda x, y: x + y,
         "-": lambda x, y: x - y,
-        "...": ...,
+        "print": lambda x, y: x,
+        "/": lambda x, y: x//y,
+        "<": lambda x, y: x < y,
+        "*": lambda x, y: x * y,
+        "~": lambda x: not x,
+        "=": lambda x, y: x == y,
+        "|": lambda x, y: x or y,
+        "^": lambda x, y: x^y,
+        "if1": lambda x, y, z: y,
+        "if2": lambda x, y, z: z,
+        "|": lambda x, y: x,
+        "|2": lambda x, y: not(x)
     }
 
 
@@ -90,9 +126,7 @@ def default_env():
 
 
 def run_module(ir: IR) -> Value:
-    """
-    Executa o módulo representado pela IR e retorna o valor calculado pela função main.
-    """
+  
 
     env = default_env()
     module = compile_module(ir, env)
